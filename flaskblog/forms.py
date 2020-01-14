2c481ee1e5571267074f174cf9aa3d9a81ae7f11
Nachiket Palkandwar <your_email@example.com>
2020-01-14 18:07:26 +0530

    On branch master Changes made after Part 10 of the tutorial series Changes to be committed: 	modified:   flaskblog/__init__.py 	modified:   flaskblog/forms.py 	modified:   flaskblog/models.py 	modified:   flaskblog/routes.py 	modified:   flaskblog/site.db 	new file:   flaskblog/static/profile_pics/2e32b4c96a8d8f10.jpg 	new file:   flaskblog/static/profile_pics/7798432669b8b3ac.jpg 	new file:   flaskblog/static/profile_pics/85ed1b444539873d.png 	new file:   flaskblog/static/profile_pics/b6e1c53325f88b74.png 	modified:   flaskblog/static/profile_pics/default.jpg 	modified:   flaskblog/templates/account.html 	modified:   flaskblog/templates/create_post.html 	modified:   flaskblog/templates/home.html 	modified:   flaskblog/templates/layout.html 	modified:   flaskblog/templates/login.html 	modified:   flaskblog/templates/post.html 	new file:   flaskblog/templates/reset_request.html 	new file:   flaskblog/templates/reset_token.html 	new file:   flaskblog/templates/user_posts.html 	new file:   posts.json 	modified:   run.py

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -19,12 +19,12 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose another one.')
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose another one.')
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -40,23 +40,41 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile picture', validators=[FileAllowed('jpg', 'png')])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. Please choose another one.')
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Please choose another one.')
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password.')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,17 +2,19 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskblog import app, db, bcrypt, mail
+from flaskblog.forms import (RegistrationForm, LoginForm, UpdateAccountForm, PostForm,
+                             RequestResetForm, ResetPasswordForm)
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-from PIL import Image
+from flask_mail import Message
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -63,10 +65,12 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
+
     return picture_fn
 
 
@@ -91,7 +95,7 @@ def account():
                            image_file=image_file, form=form)
 
 
-@app.route("/post/new", methods=['POST', 'GET'])
+@app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
@@ -99,18 +103,19 @@ def new_post():
         post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
-        flash(f"Your post has been created!", category='success')
+        flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template("create_post.html", title="New post", form=form)
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
 
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
-    return render_template('post.html', title=post.title, post=post, legend='New post')
+    return render_template('post.html', title=post.title, post=post)
 
 
-@app.route("/post/<int:post_id>/update", methods=['POST', 'GET'])
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -121,13 +126,13 @@ def update_post(post_id):
         post.title = form.title.data
         post.content = form.content.data
         db.session.commit()
-        flash(f"Your post has been updated!", category='success')
+        flash('Your post has been updated!', 'success')
         return redirect(url_for('post', post_id=post.id))
     elif request.method == 'GET':
         form.title.data = post.title
         form.content.data = post.content
-
-    return render_template("create_post.html", title="Update post", form=form, legend='Update post')
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
 
 
 @app.route("/post/<int:post_id>/delete", methods=['POST'])
@@ -138,5 +143,58 @@ def delete_post(post_id):
         abort(403)
     db.session.delete(post)
     db.session.commit()
-    flash(f"Your post has been deleted!", category='success')
+    flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message("Password reset link.", sender="noreply@demo.com",
+                  recipients=[user.email])
+    msg.body = f""" To reset your password, visit following link : 
+{url_for('reset_token',token=token, _external=True)}
+
+If you did not make this request, simply ignore this email & no changes will be made.
+"""
+    mail.send(msg)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash("An email has been sent with instructions to reset your password.", category='info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title="Reset Password", form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if not user:
+        flash("That is an invalid/expired token", category='warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been reset! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title="Reset Password", form=form)
